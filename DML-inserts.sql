05af7334f0177210e5d011f83bee323a08417629
Larissa Vardiero <larissa_vardiero@hotmail.com>
2022-10-08 20:44:41 -0300

    feat(insert): inserção de dados na tabela alunos

---
+++ DML-inserts.sql
@@ -0,0 +1,32 @@
+use proj_final_mod;
+
+#ALUNOS
+select * from alunos;
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('27048950855', 'nome1', 'sobrenome1', '1997-02-12', 'F', 'Brasileira', 'bairro1', 'cidade1', 'estado1');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('45740967999', 'nome2', 'sobrenome2', '1986-05-16', 'M', 'Brasileira', 'bairro2', 'cidade2', 'estado2');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('41847494910', 'nome3', 'sobrenome3', '1987-03-04', 'F', 'Brasileira', 'bairro3', 'cidade3', 'estado3');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('35336460537', 'nome4', 'sobrenome4', '1998-03-19', 'M', 'Brasileira', 'bairro4', 'cidade4', 'estado4');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('49245135182', 'nome5', 'sobrenome5', '1988-07-27', 'M', 'Brasileira', 'bairro5', 'cidade5', 'estado5');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('43082200637', 'nome6', 'sobrenome6', '1993-08-16', 'F', 'Brasileira', 'bairro6', 'cidade6', 'estado6');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('78583916015', 'nome7', 'sobrenome7', '1998-04-26', 'M', 'Brasileira', 'bairro7', 'cidade7', 'estado7');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('5574099567', 'nome8', 'sobrenome8', '1992-10-16', 'M', 'Brasileira', 'bairro8', 'cidade8', 'estado8');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('70133248861', 'nome9', 'sobrenome9', '1994-10-27', 'M', 'Brasileira', 'bairro9', 'cidade9', 'estado9');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('38151354544', 'nome10', 'sobrenome10', '1989-05-10', 'F', 'Brasileira', 'bairro10', 'cidade10', 'estado10');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('36812338856', 'nome11', 'sobrenome11', '1990-08-31', 'M', 'Brasileira', 'bairro11', 'cidade11', 'estado11');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('73807819454', 'nome12', 'sobrenome12', '1992-10-15', 'M', 'Brasileira', 'bairro12', 'cidade12', 'estado12');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('67170163840', 'nome13', 'sobrenome13', '1984-07-23', 'F', 'Brasileira', 'bairro13', 'cidade13', 'estado13');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('21447355350', 'nome14', 'sobrenome14', '1984-04-18', 'M', 'Brasileira', 'bairro14', 'cidade14', 'estado14');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('50018267283', 'nome15', 'sobrenome15', '1998-05-25', 'M', 'Brasileira', 'bairro15', 'cidade15', 'estado15');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('93224341743', 'nome16', 'sobrenome16', '1987-10-12', 'M', 'Brasileira', 'bairro16', 'cidade16', 'estado16');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('17529555065', 'nome17', 'sobrenome17', '1991-07-08', 'M', 'Brasileira', 'bairro17', 'cidade17', 'estado17');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('23347646827', 'nome18', 'sobrenome18', '1993-03-07', 'F', 'Brasileira', 'bairro18', 'cidade18', 'estado18');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('20717424657', 'nome19', 'sobrenome19', '1996-03-13', 'M', 'Brasileira', 'bairro19', 'cidade19', 'estado19');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('14716532020', 'nome20', 'sobrenome20', '1989-12-09', 'F', 'Brasileira', 'bairro20', 'cidade20', 'estado20');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('49103180592', 'nome21', 'sobrenome21', '1985-01-11', 'M', 'Brasileira', 'bairro21', 'cidade21', 'estado21');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('13805077401', 'nome22', 'sobrenome22', '1988-09-21', 'F', 'Brasileira', 'bairro22', 'cidade22', 'estado22');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('45673806115', 'nome23', 'sobrenome23', '1997-08-18', 'M', 'Brasileira', 'bairro23', 'cidade23', 'estado23');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('97628598568', 'nome24', 'sobrenome24', '1993-06-09', 'F', 'Brasileira', 'bairro24', 'cidade24', 'estado24');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('68960602499', 'nome25', 'sobrenome25', '1996-02-04', 'M', 'Brasileira', 'bairro25', 'cidade25', 'estado25');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('17177464078', 'nome26', 'sobrenome26', '1996-10-17', 'F', 'Brasileira', 'bairro26', 'cidade26', 'estado26');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('30642159947', 'nome27', 'sobrenome27', '1987-05-29', 'F', 'Brasileira', 'bairro27', 'cidade27', 'estado27');
+INSERT INTO alunos (CPF, NOME, SOBRENOME, DATA_NASCIMENTO, GENERO, NACIONALIDADE, BAIRRO, CIDADE, ESTADO) VALUES ('54064447294', 'nome28', 'sobrenome28', '1992-08-28', 'M', 'Brasileira', 'bairro28', 'cidade28', 'estado28');
